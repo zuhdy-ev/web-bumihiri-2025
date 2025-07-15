1d6215ab9634fdfc3ed453a33182386446c7af51
Fathurrahman Kafi <fathurrahmankafi@Fathurrahmans-MacBook-Air.local>
2025-07-16 08:31:33 +0900

    tambah vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,38 +1,6 @@
-// import './globals.css'
-// import { Inter } from 'next/font/google'
-// import Footer from '@/components/layout/Footer'
-// import Head from 'next/head'
-
-// const inter = Inter({ subsets: ['latin'] })
-
-// export const metadata = {
-//   title: 'Bumi Hiri 2025 | KKN-PPM UGM Periode 2 2025',
-//   description: 'Official Website Bumi Hiri 2025',
-//   icons: {
-//     icon: [
-//       { url: '/favicon.ico' },
-//       { url: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
-//     ],
-//   },
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <Head>
-//         <link rel="icon" href="/favicon.ico" sizes="any" />
-//       </Head>
-//       <body className="font-made-tommy bg-white overflow-x-hidden">
-//         {children}
-//         {/* <Footer /> */}
-//       </body>
-//     </html>
-//   )
-// }
-
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/layout/Footer'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import Script from 'next/script'
 import Analytics from '@/components/Analytics'
 
@@ -77,6 +45,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="font-made-tommy bg-white overflow-x-hidden">
+        <SpeedInsights />
         <Analytics />
         {children}
         {/* <Footer /> */}
